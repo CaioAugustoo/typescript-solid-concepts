fc8bf8f44a02bad4f62de80d2b2ede23538fc808
Caio Augusto <caioamfr@gmail.com>
2021-12-22 22:55:20 -0300

    ISP - Interface Segregation Principle (Princípio da Segregação de Interface) #1

---
+++ src/ISP/classes/customer.ts
@@ -0,0 +1,15 @@
+import {
+  IndividualCustomerProtocol,
+  EnterpriseCustomerProtocol,
+} from "./interfaces/customer";
+
+export class IndividualCustomer implements IndividualCustomerProtocol {
+  constructor(
+    public firstName: string,
+    public lastName: string,
+    public cpf: string
+  ) {}
+}
+export class EnterpriseCustomer implements EnterpriseCustomerProtocol {
+  constructor(public name: string, public cnpj: string) {}
+}
---
+++ src/ISP/classes/product.ts
@@ -0,0 +1,5 @@
+import { Item } from "./interfaces/cart-item";
+
+export class Product implements Item {
+  constructor(public name: string, public price: number) {}
+}
